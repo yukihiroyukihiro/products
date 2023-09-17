90d7d549695f53ca68f7ff56019e8b5fdafc222d
yukihiro <currydrum@gmail.com>
2023-09-17 20:19:45 +0800

    add check file exist

--- products_read_then_write.py
+++ products_read_then_write.py
@@ -1,15 +1,26 @@
-#讀取檔案
-products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #結束這一回，直接去下一回
-		name, price = line.strip().split(',') #strip() -> 去掉空格跟'\n'換行
-		                                      #split',' -> 將字串逗號分割，變成清單存入s.
-		products.append([name, price])
-print(products)
+import os #operating system
 
-#二維清單
+products = []
+
+#讀取檔案
+if os.path.isfile('products.csv'): #檢查相對路徑有沒有這個檔案
+	print('有找到檔案！')
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #結束這一回，直接去下一回
+			name, price = line.strip().split(',') #strip() -> 去掉空格跟'\n'換行
+			                                      #split',' -> 將字串逗號分割，變成清單存入s.
+			products.append([name, price])
+	print(products)
+
+else:
+	print('找不到檔案...')
+
+
+
+
+#輸入商品跟價錢，二維清單
 while True:
 	name = input('請輸入商品名稱：')
 	if name == 'q':
@@ -18,11 +29,6 @@ while True:
 	products.append(([name, price]))
 print('印出清單裡所有的商品與價格：', products)#印出清單裡所有的商品與價格
 
-#用for loop一行一行印出products
-print('------------------')
-print('一行一行印出清單：')
-for p in products:
-	print(p)
 
 
 #用for loop一行一行印出products
